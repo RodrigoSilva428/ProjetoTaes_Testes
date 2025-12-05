52b0caa8e7b59af4f6c0a205dd6122c3eeb059c8
RodrigoSilva428 <rodrigofsilva428@gmail.com>
2025-12-05 20:14:32 +0000

    Funcs gerais

---
+++ Keywords/loginFunctions/loginFunctions.groovy
@@ -0,0 +1,44 @@
+package loginFunctions
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+import generalFunctions.btnAndElementsFunctions as Elements
+
+public class loginFunctions {
+
+	@Keyword
+	def openApp() {
+
+		// Path to your APK (change as needed)
+		String appPath = GlobalVariable.AppPath
+
+		// Starts the app
+		Mobile.startApplication(appPath, false)
+
+		// Optional - Wait for login page element
+		  Elements.checkElementPresenceByName('LoginPage/usernameField', 20)
+	}
+
+
+	@Keyword
+	def closeApp() {
+		Mobile.closeApplication()
+	}
+}
